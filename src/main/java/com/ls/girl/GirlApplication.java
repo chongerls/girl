fae85267a6560666ae9fd60b6d1198d7fe286670
lishan10 <lishan10@meituan.com>
2018-07-27 11:25:30 +0800

    update6

--- src/main/java/com/ls/girl/GirlApplication.java
+++ src/main/java/com/ls/girl/GirlApplication.java
@@ -2,9 +2,16 @@ package com.ls.girl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GirlApplication {
+public class GirlApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(GirlApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GirlApplication.class, args);
